10724d054a7751d885d72553ef26273b48d23938
李小明 <alx696@gmail.com>
2021-08-16 15:58:06 +0800

    构建脚本判断gomobile

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ go mod download
 
 mkdir -p build
 
-#echo "Linux"
-#go build -o "build/${NAME}-linux"
-#
-#echo "Windows"
-#sudo apt-get install -y gcc-multilib gcc-mingw-w64
-#GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o "build/${NAME}-windows"
+echo "Linux"
+go build -o "build/${NAME}-linux"
+
+echo "Windows"
+sudo apt-get install -y gcc-multilib gcc-mingw-w64
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o "build/${NAME}-windows"
 
 echo "Android"
 echo "NDK路径: ${ANDROID_NDK_HOME}"
