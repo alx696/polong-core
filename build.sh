135744fb795938c75a2a28522b895c653e5dc1bc
李小明 <alx696@gmail.com>
2021-08-16 15:36:07 +0800

    测试

--- build.sh
+++ build.sh
@@ -7,16 +7,25 @@ go mod tidy
 go mod download
 
 mkdir -p build
-echo "Linux"
-go build -o "build/${NAME}-linux"
 
-echo "Windows"
-sudo apt install -y gcc-multilib gcc-mingw-w64
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o "build/${NAME}-windows"
+#echo "Linux"
+#go build -o "build/${NAME}-linux"
+#
+#echo "Windows"
+#sudo apt-get install -y gcc-multilib gcc-mingw-w64
+#GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o "build/${NAME}-windows"
 
 echo "Android"
-go get golang.org/x/mobile/cmd/gomobile
-gomobile init
+if [ -z "$ANDROID_NDK_HOME" ]; then
+  echo "没有安装NDK"
+  wget https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
+  unzip android-ndk-r21e-linux-x86_64.zip
+  export ANDROID_NDK_HOME="$(pwd)/android-ndk-r21e"
+  go get golang.org/x/mobile/cmd/gomobile
+  gomobile init
+else
+  echo "已经安装NDK"
+fi
+echo "NDK路径: ${ANDROID_NDK_HOME} 开始编译aar"
 gomobile bind -target=android/arm64,android/arm -o "build/${NAME}.aar" ./kc ./qc
-
 rm "build/${NAME}-sources.jar"
