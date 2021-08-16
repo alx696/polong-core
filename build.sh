2e9b340258aaf98d1498b52313a1c958e6b23dd3
李小明 <alx696@gmail.com>
2021-08-16 15:49:35 +0800

    测试

--- build.sh
+++ build.sh
@@ -16,16 +16,13 @@ mkdir -p build
 #GOOS=windows GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc CXX=x86_64-w64-mingw32-g++ go build -o "build/${NAME}-windows"
 
 echo "Android"
-if [ -z "$ANDROID_NDK_HOME" ]; then
-  echo "没有安装NDK"
-  wget https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
-  unzip android-ndk-r21e-linux-x86_64.zip
-  export ANDROID_NDK_HOME="$(pwd)/android-ndk-r21e"
+echo "NDK路径: ${ANDROID_NDK_HOME}"
+if [ -z "$(which gomobile)" ]; then
+  echo "没有安装gomobile"
   go get golang.org/x/mobile/cmd/gomobile
   gomobile init
 else
-  echo "已经安装NDK"
+  echo "已经安装gomobile"
 fi
-echo "NDK路径: ${ANDROID_NDK_HOME} 开始编译aar"
 gomobile bind -target=android/arm64,android/arm -o "build/${NAME}.aar" ./kc ./qc
 rm "build/${NAME}-sources.jar"
